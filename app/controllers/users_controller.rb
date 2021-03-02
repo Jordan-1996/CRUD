c53baba5753554272759500a219c8523182aeafd
jordan <jordansandoval1819@gmail.com>
2021-03-02 16:39:58 -0600

    crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,45 @@
 class UsersController < ApplicationController
-  def form
+
+  before_action :set_user, only: [:edit,:update,:destroy]
+
+  def new
     @user = User.new  #la ruta busca al controlador 
   end
 
   def create
-    @user = User.new(name: params["name"], email:params["email"])
+      @user = User.new(name: params["name"], email:params["email"])
     if @user.save
      redirect_to '/users/index'
     else 
-      render 'form'   #vuelve a cargar la lista form con la lista de errores 
+      render 'new'   #vuelve a cargar la lista form con la lista de errores 
     end 
   end 
 
   def index
    @users = User.all     
   end
+
+  def edit 
+  end 
+
+  def update 
+    
+    if @user.update(name: params["name"], email:params["email"])
+     redirect_to '/users/index'
+    else 
+      render 'edit'
+    end
+  end 
+  
+  def destroy 
+    
+    @user.destroy 
+    redirect_to '/users/index'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])  
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
-  get 'users/form'  #ruta que nos lleva al formulario 
+  get 'users/new'  #ruta que nos lleva al formulario 
 
   post 'users/create'  # el post trae la informacion
   
   get 'users/index'
+
+  get 'users/:id/edit', to: 'users#edit', as: 'users_edit' 
+  
+  post 'users/:id', to: 'users#update', as: 'users_update'
+
+  delete 'users/:id', to: 'users#destroy', as: 'users_destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
